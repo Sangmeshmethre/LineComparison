ba0236f61fc7fe5d90f085991bbd5c7de14858ef
Sangmesh Methre <ssmcmethre3080@gmail.com>
2023-01-05 17:15:08 +0530

    Euality Of Two Line

---
+++ LineComparison.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonUC2
+{
+    public  class LineComparison
+    {
+        public static void TwoLineAreEqual()
+        {
+            Console.WriteLine("Enter Value Of: X1");
+            int X1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Value Of : Y1");
+            int Y1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Value Of : X2");
+            int X2=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Value Of : Y2");
+            int Y2 = Convert.ToInt32(Console.ReadLine());
+
+            //Using Double To Get Square Root Value In Double Decimal Point
+            double Length = Math.Sqrt((Math.Pow((X2-X1), 2) + Math.Pow((Y2-Y1), 2)));
+            Console.WriteLine("Length Of The Line Is :" + Length);
+
+            Console.ReadLine();
+        }
+    }
+}
